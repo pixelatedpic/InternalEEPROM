08697ad460242b06f362128f6324818daa022827
Mohamed Afzal <mohamedafzal@gmail.com>
2016-12-25 11:59:07 +0500

    Update EEPROM_RW.ino

--- EEPROM_RW.ino
+++ EEPROM_RW.ino
@@ -10,19 +10,37 @@ struct config_t
 void setup()
 {
     Serial.begin(9600);
- /*   conf.mRead = 1234567;
-    EEPROM_writeAnything(0, conf);
+//    conf.mRead = 7991206;
+//    EEPROM_writeAnything(0, conf);
     
     //Serial.println(EEPROM_writeAnything(0, configuration.meterreading));
- */   
+    delay(500);
     EEPROM_readAnything(0, conf);
     
     delay(500);
-    
+    Serial.print("This is a long: ");
     Serial.println(conf.mRead);
+    //char buf[7];
+    //sprintf(buf,"%1u",conf.mRead);
+    //ltoa(conf.mRead,buf,7);
+    String t = String(conf.mRead);
     
-     delay(2000);
+     delay(500);
+     Serial.print("This is a string: ");
+     Serial.println(t);
+     String rr = "7991206";
+     if(t == rr){
+       
+       Serial.println("Equal");
+     }else Serial.println("Not Equal");
     // ...
+    long r = t.toInt();
+    Serial.print("This is a long again: ");
+    Serial.println(r);
+    if(conf.mRead = r){
+       
+       Serial.println("Equal");
+     }else Serial.println("Not Equal");
 }
 void loop()
 {
